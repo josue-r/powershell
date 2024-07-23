4c652d9c02531a3623e47597b93c07ead595182f
Josue-RS <josue.ruiz@wizeline.com>
2024-07-23 14:53:49 -0600

    remove duplicate code

--- store-container-manager.ps1
+++ store-container-manager.ps1
@@ -58,26 +58,6 @@ function Get-LocalDockerContainers {
     return $containers
 }
 
-function Get-LocalDockerContainers {
-    $containers = @()
-    # This function uses the Docker CLI to get a list of all containers and their statuses from the current Docker host.
-    docker ps -a --format "{{.Image}};{{.Status}}" | ForEach-Object {
-        $repoAndTag = $_.Split(";")
-        $repositoryFull = $repoAndTag[0].Split("/")[-1]
-        $repositoryName = $repositoryFull.Split("/")[-1].Split(":")[0]
-        $status = $repoAndTag[-1]
-        if ($global:mandatoryImages -contains $repositoryName) {
-            $containers += [PSCustomObject]@{
-                repositoryFull = $repositoryFull
-                Status = $status
-            }
-        }
-    }
-    return $containers
-}
-
-
-
 function Get-PortMapping {
     param($repositoryName)
     switch ($repositoryName) {
